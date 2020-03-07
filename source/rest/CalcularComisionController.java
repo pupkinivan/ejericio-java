ea7d2c32b6589bd8127b102ef14572033635130d
Ivan Pupkin <46005557+pupkinivan@users.noreply.github.com>
2020-03-06 21:41:29 -0300

    Emprolijancia

--- source/rest/CalcularComisionController.java
+++ source/rest/CalcularComisionController.java
@@ -1,15 +1,11 @@
 package com.example.restservice;
 
-// import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalcularComisionController {
-    // private static final String template = "Hello, %s!";
-    // private static final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/getComision")
     public CalcularComision getComision(
